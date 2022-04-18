430d09f6310eeeb10325187cf6154a68e25242e4
insaiyancvk <cvamshik1@gmail.com>
2022-04-19 01:04:21 +0530

    revamp the interface

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from sql import stu_check_credentials, stu_courses_registered, join_class, stu_register, teach_check_credentials, teach_courses_registered, get_attendance, teach_register
+from utils import pass_inp, get_who, get_login_register, yn_prompt
+# from PyInquirer.prompt import prompt
 
 
-who = input('Login as Student or Teacher')
+who = get_who()
 
-if who.lower() in ['s','student']:
+if who == 'student':
 
-    ch1 = input('Login/Register')
+    ch1 = get_login_register()
 
-    if ch1.lower() in ['login','l']:
-        mailid = input("Enter your college mail ID")
-        password = input("Enter the password")
+    if ch1 == 'login':
+
+        mailid = input("Enter your college mail ID: ")
+        password = pass_inp(f'Enter the password: ', 'passw')['passw']
+
         check_creds = stu_check_credentials(mailid, password)
+        
         if check_creds:
             # retrieve the courses registered by the student with the mail id
             courses = '\n'.join(stu_courses_registered(mailid))
@@ -18,35 +23,43 @@ if who.lower() in ['s','student']:
             class_join = input('Enter the index number corresponding to course to join that class: ')
             class_join -= 1
             join_class(mailid, courses[class_join])
+        
         elif not check_creds:
             print('Wrong credentials :(')
+        
         elif check_creds == -1:
-            yn = input('mail id doesn\'t exist. would you like to register? (y/n) ')
+            
+            yn = yn_prompt('Mail ID doesn\'t exist. Would you like to register? ','yn')['yn']
             # Call registration funtion
-            if yn.lower() == 'y':
+            
+            if yn:
                 d = stu_register()
                 if d == 1:
                     print('Account registered successfully')
                 elif d == -1:
                     print('Account already exists. Please login')
+            
             else:
                 print('Exiting.')
                 quit()
     
-    elif ch1.lower() in ['register','r']:
+    elif ch1 == 'register':
+       
         d = stu_register()
+
         if d == 1:
             print('Account created successfully.')
-        elif d==-1:
+        elif d == -1:
             print('Account already exists. Please login')
 
-elif who.lower() in ['t','teacher']:
+elif who == 'teacher':
     
-    ch1 = input('Login/Register')
+    ch1 = get_login_register()
 
-    if ch1.lower() in ['login','l']:
+    if ch1 == 'login':
+
         mailid = input('Enter your college mail ID: ')
-        password = input('Enter the password')
+        password = pass_inp(f'Enter the password: ', 'passw')['passw']
         check_creds = teach_check_credentials(mailid, password)
         
         if check_creds:
@@ -56,22 +69,32 @@ elif who.lower() in ['t','teacher']:
             attendance = input('Enter the index number corresponding to course for retrieving the attendance: ')
             attendance -= 1
             path = get_attendance(courses[attendance])
+
         elif not check_creds:
             print('Wrong credentials')
+
         elif check_creds == -1:
-            yn = input('mail id doesn\'t exist. would you like to register? (y/n)')
-            if yn.lower() == 'y':
+
+            yn = yn_prompt('Mail ID doesn\'t exist. Would you like to register? ','yn')['yn']
+
+            if yn:
+
                 d = teach_register()
+
                 if d == 1:
                     print('Account registered successfully')
+
                 elif d == -1:
                     print('Account already exists. Please login')
+
             else:
                 print('Exiting.')
                 quit()
     
-    if ch1.lower() in ['register','r']:
+    if ch1 == 'register':
+
         d = teach_register()
+
         if d == 1:
             print('Account created successfully')
         elif d == -1:
